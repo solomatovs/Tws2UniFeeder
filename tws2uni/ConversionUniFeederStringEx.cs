bb64ab38f74bcf13f57728f638da05679982840a
Соломатов Сергей Иванович <SolomatovS@gmail.com>
2019-12-17 15:24:47 +0300

    test

---
+++ tws2uni/ConversionUniFeederStringEx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.IO;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace tws2uni
+{
+    public static class ConversionUniFeederStringEx
+    {
+        public static byte[] ToUniFeederByteArray(this string s) => Encoding.UTF8.GetBytes(s + "\0");
+
+        public static IEnumerable<string> ToUniFeederStrings(this IEnumerable<byte> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            using (var ms = new MemoryStream())
+            {
+                foreach (var b in source)
+                {
+                    if (b == 0)
+                    {
+                        yield return GetString(ms);
+                        ms.SetLength(0);
+                    }
+                    else
+                        ms.WriteByte(b);
+                }
+                if (ms.Position != 0)
+                    throw new InvalidDataException("ToStrings: no termination(1).");
+            }
+        }
+
+        public static IObservable<string> ToUniFeederStrings(this IObservable<byte> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            var ms = new MemoryStream();
+
+            return Observable.Create<string>(observer =>
+            {
+                return source.Subscribe(
+                    onNext: b =>
+                    {
+                        if (b.isUniFeederDivider())
+                        {
+                            observer.OnNext(GetString(ms));
+                            ms.SetLength(0);
+                        }
+                        else
+                            ms.WriteByte(b);
+                    },
+                    onError: observer.OnError,
+                    onCompleted: () =>
+                    {
+                        if (ms.Position == 0)
+                            observer.OnCompleted();
+                        else
+                            observer.OnError(new InvalidDataException("ToStrings: no termination(2)."));
+                    });
+            });
+        }
+
+        private static string GetString(in MemoryStream ms) => Encoding.UTF8.GetString(ms.GetBuffer(), 0, (int)ms.Position);
+        private static bool isUniFeederDivider(this byte c) => c == 13;
+    }
+}
--- tws2uni/UniFeedConsumer.cs
+++ tws2uni/UniFeedConsumer.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using RxSockets;
 
 namespace tws2uni
@@ -19,66 +20,46 @@ namespace tws2uni
 
         public UniFeedConsumer(IBackgroundQueue<TwsTick> taskQueue, ILoggerFactory loggerFactory)
         {
-            this.server = (new IPEndPoint(IPAddress.Parse("0.0.0.0"), 3309)).CreateRxSocketServer(loggerFactory.CreateLogger<RxSocketServer>());
+            this.server = (new IPEndPoint(IPAddress.Parse("0.0.0.0"), 5001)).CreateRxSocketServer(loggerFactory.CreateLogger<RxSocketServer>());
             this.taskQueue = taskQueue;
             logger = loggerFactory.CreateLogger<UniFeedConsumer>();
         }
 
-        public async override Task StartAsync(CancellationToken cancellationToken)
-        {
-            await Task.Run(() => BootstrapServer(server));
-
-            
-        }
-
-        public async override Task StopAsync(CancellationToken cancellationToken)
-        {
-            await Task.Run(() => server.Dispose());
-
-            
-        }
-
         protected async override Task ExecuteAsync(CancellationToken cancellationToken)
         {
-            logger.LogInformation("UniFeedConsumer is starting.");
-            await base.StartAsync(cancellationToken);
-
-            while (!cancellationToken.IsCancellationRequested)
-            {
-                var tick = await taskQueue.DequeueAsync(cancellationToken);
-                logger.LogInformation($"{tick}");
-            }
-
-            await base.StopAsync(cancellationToken);
-            logger.LogInformation("UniFeedConsumer is stopping.");
+            await UniFeederServerLoop(this.server, cancellationToken);
+            this.server.Dispose();
         }
 
-        protected void BootstrapServer(IRxSocketServer server)
+        protected async Task UniFeederServerLoop(IRxSocketServer server, CancellationToken cancellationToken)
         {
-            // Start accepting connections from clients.
-            server.AcceptObservable.Subscribe(
-                onNext: async acceptClient => {
-                    var hello_message = @"Universal DDE Connector 9.00
+            var accept = await server.AcceptObservable.ToTask();
+            @"Universal DDE Connector 9.00
 Copyright 1999 - 2008 MetaQuotes Software Corp.
-Login: ";
-                    acceptClient.Send(hello_message.ToByteArray());
+Login: ".ToByteArray().SendTo(accept);
 
-                    var t = await acceptClient.ReceiveObservable.ToStrings().Take(1).FirstAsync();
-                    logger.LogInformation(t);
-                    acceptClient.ReceiveObservable.ToStrings().Subscribe(message =>
-                    {
-                        // Echo received messages back to the client.
-                        // acceptClient.Send(message.ToByteArray());
-                        logger.LogInformation(message);
-                    }
-                    );
+            accept.ReceiveObservable.ToUniFeederStrings().Subscribe(login =>
+            {
+                logger.LogInformation($"login reveived: '{login}'");
 
-                },
-                onError: error => {
-                    logger.LogInformation(error.Message);
-                },
-                onCompleted: () => logger.LogInformation("complite")
-            );
+                accept.Send("> Password: ".ToByteArray());
+                accept.ReceiveObservable.ToUniFeederStrings().Subscribe(onNext: password =>
+                {
+                    logger.LogInformation($"password reveived: '{password}'");
+                    accept.Send("> Access granted".ToByteArray());
+
+                    ProccessLoop(cancellationToken).ConfigureAwait(false);
+                });
+            });
+        }
+
+        protected async Task ProccessLoop(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var tick = await taskQueue.DequeueAsync(token);
+                logger.LogInformation(tick.ToString());
+            }
         }
     }
 }
